123a7b2203b7a90e324b23879db04849782d35e8
sanlugaTec <luis.garcia@tec.mx>
2019-02-26 09:30:31 -0600

    primer commit

---
+++ motorDC.ino
@@ -0,0 +1,24 @@
+int IN1 = 5;  // Control de velocidad
+int IN2 = 6;  // Motor
+                      
+void setup() 
+{ 
+    Serial.begin (9600);
+    pinMode(IN2, OUTPUT);   
+} 
+
+void loop() 
+{ 
+    int potentiometerVal = analogRead(A0);
+    Serial.println(potentiometerVal);
+    activa_motor(potentiometerVal);
+}
+
+void activa_motor(int potValue)
+{
+      int mappedVal = map(potValue,0,1022,0,255);
+      // Adelante
+      digitalWrite(IN2,HIGH);         
+      analogWrite(IN1, mappedVal);   //Control de velocidad PWM
+      delay(30); 
+}
